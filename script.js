7f81e2d7bb2850f1cf867c8bc3da57058074080a
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-06-06 12:36:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
-// function volume_sphere() {
-//     //Write your code here
-// 	var volume;
-// 		var radius = document.getElementById("radius").value;
-// 		radius = (4/3)*Math.PI*Math.pow(radius,3);
-// 		volume = volume.toFixed(4);
-// 	document.getElementById("volume").value = volume;
-// 	return false;
-  
-// } 
+function volume_sphere() {
+    //Write your code here
+  let radius=document.getElementById('radius').value;
+  console.log(radius);
+  let ans;
+  if(radius){
+  ans=(4*3.14*radius*radius*radius)/3;
+  }
+  let volume=document.getElementById('volume');
+//   console.log(volume);
+if(ans!=undefined){
+    volume.value=ans;
+}
 
-// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+} 
+
+window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 
 // -----------------
-let clickfn = document.getElementById("submit");
-clickfn.addEventListener("click", getVol);
+// let clickfn = document.getElementById("submit");
+// clickfn.addEventListener("click", getVol);
 
-function getVol(){
-  let radius = document.getElementById("radius").value;
-  let volume = 4/3*3.14*radius*radius*radius;
-  console.log(volume);
-  document.getElementById("volume").innerText = volume;
-}
-// getVol();
+// function getVol(){
+//   let radius = document.getElementById("radius").value;
+//   let volume = 4/3*3.14*radius*radius*radius;
+//   console.log(volume);
+//   document.getElementById("volume").innerText = volume;
+// }
+// // getVol();
