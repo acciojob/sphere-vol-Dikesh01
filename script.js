3134b292208332dd677144e65c1e14b2f7515a24
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-06-06 12:56:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function volume_sphere() {
-    //Write your code here
-  let radius=document.getElementById('radius').value;
-  console.log(radius);
-  let ans;
-  if(radius){
-  ans=(4*3.14*radius*radius*radius)/3;
-  }
-  let volume=document.getElementById('volume');
-//   console.log(volume);
-if(ans!=undefined){
-    volume.value=ans;
+    let radius = document.getElementById('radius').value;
+    console.log(radius);
+    let ans;
+    if (radius) {
+        ans = (4 * Math.PI * Math.pow(radius, 3)) / 3;
+    }
+    let volume = document.getElementById('volume');
+    if (ans != undefined) {
+        volume.value = ans;
+    }
 }
-
-} 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
 
