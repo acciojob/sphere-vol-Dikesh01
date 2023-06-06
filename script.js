6a5ca129777230a466a2dd139e589a5d80265899
Dikesh01 <117905520+Dikesh01@users.noreply.github.com>
2023-06-06 09:23:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-function volume_sphere() {
-    //Write your code here
-	var volume;
-		var radius = document.getElementById("radius").value;
-		radius = (4/3)*Math.PI*Math.pow(radius,3);
-		volume = volume.toFixed(4);
-	document.getElementById("volume").value = volume;
-	return false;
+// function volume_sphere() {
+//     //Write your code here
+// 	var volume;
+// 		var radius = document.getElementById("radius").value;
+// 		radius = (4/3)*Math.PI*Math.pow(radius,3);
+// 		volume = volume.toFixed(4);
+// 	document.getElementById("volume").value = volume;
+// 	return false;
   
-} 
+// } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+
+// -----------------
+let clickfn = document.getElementById("submit");
+clickfn.addEventListener("click", getVol);
+
+function getVol(){
+  let radius = document.getElementById("radius").value;
+  let volume = 4/3*3.14*radius*radius*radius;
+  console.log(volume);
+  document.getElementById("volume").innerText = volume;
+}
+// getVol();
